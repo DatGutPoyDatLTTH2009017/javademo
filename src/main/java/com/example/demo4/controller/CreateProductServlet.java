19d57038895c9bcbd877e22b599aa17863a4da98
DESKTOP-0K5Q27J\uinhi <datltth2009017@fpt.edu.vn>
2022-06-02 08:08:32 +0700

    update

--- src/main/java/com/example/demo4/controller/CreateProductServlet.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo4.controller;
-
-public class CreateProductServlet {
-}
--- src/main/java/com/example/demo4/controller/DeleteProductServlet.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo4.controller;
-
-public class DeleteProductServlet {
-    public static class ListProductServlet {
-    }
-
-    public static class UpdateProductServlet {
-    }
-}
